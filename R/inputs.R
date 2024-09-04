669f206f7053a6a5961df89decc83c79207c0507
Ben Williams <ben.williams@noaa.gov>
2024-09-04 15:44:44 -0800

    correct file

--- R/inputs.R
+++ R/inputs.R
@@ -697,7 +697,7 @@ bts_length_comp <- function(year, area = "goa", lenbins = NULL, bysex = NULL, al
     lenbins =  vroom::vroom(here::here(year, "data", "user_input", lenbins), delim = ",")$len_bins
   }
 
-  vroom::vroom(here::here(year, "data", "raw", paste0(area, "_bts_sizecomp_data.csv"))) %>%
+  vroom::vroom(here::here(year, "data", "raw", paste0(area, "_bts_length_data.csv"))) %>%
     dplyr::rename_with(tolower) %>%
     dplyr::filter(!is.na(length)) %>%
     dplyr::mutate(length = length / 10) -> dat
